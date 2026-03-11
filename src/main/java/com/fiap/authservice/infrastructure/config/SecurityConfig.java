bea910047860bb77db6e2573ec6cf567e0288d3d
Leonardo Fujimura <leonardo.fujimura123@gmail.com>
2026-03-11 02:55:48 -0300

    fix

--- src/main/java/com/fiap/authservice/infrastructure/config/SecurityConfig.java
+++ src/main/java/com/fiap/authservice/infrastructure/config/SecurityConfig.java
@@ -29,8 +29,8 @@ public class SecurityConfig {
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
-                                "/auth/login",
-                                "/auth/register",
+                                "/api/auth/login",
+                                "/api/auth/register",
                                 "/v3/api-docs",
                                 "/v3/api-docs/**",
                                 "/swagger-ui/**",
